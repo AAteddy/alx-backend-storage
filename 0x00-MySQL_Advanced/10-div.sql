0cd13fb8e80feff481226bc8ba70d35ca495dc66
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-10 12:39:25 +0300

    [Fixed typo issue] on 10-div.sql file

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -10,8 +10,9 @@
 -- b, INT
 -- And returns a / b or 0 if b == 0
 
+
 DELIMITER //
-CREATE FUNCTION afeDiv(a INT, b INT) RETURNS FLOAT
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
     RETURN IF(b = 0, 0, a / b);
 END //
