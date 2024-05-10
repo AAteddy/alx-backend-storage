bec49f8b7f325414c7272edfadbb031de1bd2845
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-10 12:31:07 +0300

    Fixed issues

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -11,9 +11,9 @@
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
-BEGIN 
+BEGIN
     UPDATE users
-    SET avg_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
     WHERE id = user_id;
 END;//
 DELIMITER ;
