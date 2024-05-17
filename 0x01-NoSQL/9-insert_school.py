1d2051f37c4604b6c847eaee86fe1838c8a237c3
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-17 12:59:20 +0300

    [Fix error] replaced insert with insert_one

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -12,4 +12,4 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new doc to a collection"""
-    return mongo_collection.insert(kwargs).inserted_id
+    return mongo_collection.insert_one(kwargs).inserted_id
